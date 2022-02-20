75c4ae43e6fd39c670132c72a7e8f90d1396da6d
VPSAccountPieces <100085517+VPSAccountPieces@users.noreply.github.com>
2022-02-20 21:58:07 +0000

    cars for this fivem server...

---
+++ [cars]/urp_cars/stream/[Police]/[CUSTOMS]/config.lua
@@ -0,0 +1,138 @@
+Config = {}
+
+-- Each time you add a new .xml file, you MUST include it below!
+Config.ELSFiles = {
+--  'Example.xml',
+    "AMBULANCE.xml",
+    "fp1.xml", 
+    "fp2.xml",
+    "fp3.xml",
+    "fp4.xml",
+    "fp5.xml",
+    "fp6.xml",
+    "fp7.xml",
+    "fp8.xml",
+    "fp9.xml",
+    "fp10.xml",
+    "fp11.xml",
+    "fp12.xml",
+    "fp13.xml",
+    "fp14.xml",
+    "fp15.xml",
+    "fp16.xml",
+    "fp17.xml",
+    "arv1.xml",
+    "arv2.xml",
+    "arv3.xml",
+    "arv4.xml",
+    "aircarrier1.xml",
+    "aircarrier2.xml",
+    "area1.xml",
+    "area2.xml",
+    "rtpc1.xml",
+    "rtpc2.xml",
+    "rtpc3.xml",
+    "rtpc4.xml",
+    "rtpc5.xml",
+    "rtpc6.xml",
+    "rtpc7.xml",
+    "rtpc8.xml",
+    "rtpc9.xml",
+    "seg1.xml",
+    "tsg1.xml",
+    "ur1.xml",
+    "rbfrs10.xml",
+    "rpt1.xml",
+    "rpt2.xml",
+    "rpt3.xml",
+    "rpt4.xml",
+    "rpt5.xml",
+    "rpt6.xml",
+    "rpt7.xml",
+    "rpt8.xml",
+    "rpt9.xml",
+    "rpt10.xml",
+    "scas1.xml",
+    "scas2.xml",
+    "scas3.xml",
+    "scas4.xml",
+    "scas5.xml",
+    "scas6.xml",
+    "scas7.xml",
+    "scas8.xml",
+    "scas9.xml",
+    "scas10.xml",
+    "SHERIFF2.xml",
+    "RIOT.xml",
+    "POLICEB.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml", 
+    "ert2.xml",
+}
+
+-- Whether indicators should be enabled or not. (Controlled with Left & Right arrows by default)
+Config.Indicators = true
+
+-- If you are using server-sided sirens, uncomment below...
+Config.AudioBanks = {
+    "DLC_WMSIRENS\\SIRENPACK_ONE"
+}
+
+-- Enables the horn beeps on siren activation
+Config.HornBlip = true
+
+-- Enables/Disables ELS beeps
+Config.Beeps = false
+
+-- Whether controllers are supported for controlling ELS.
+Config.ControllerSupport = true
+
+-- Keybinds for each command. Key codes can be found at https://docs.fivem.net/docs/game-references/controls/
+Config.KeyBinds = {
+    PrimaryLights = 85,
+    SecondaryLights = 311,
+    MiscLights = 182,
+    ActivateSiren = 19,
+    NextSiren = 45,
+    Siren1 = 157,
+    Siren2 = 158,
+    Siren3 = 160,
+    Siren4 = 164
+}
---
+++ [cars]/vny-pursuitmode/config.lua
@@ -0,0 +1,22 @@
+Veny = {}
+
+Veny.PursuitCar1 = 1989174164           -- Type your pursuit car model --
+Veny.PursuitCar2 = 350874832         -- Type your pursuit car model --
+Veny.PursuitCar3 = 14057414           -- Type your pursuit car model --
+Veny.PursuitCar4 = 2005850441           -- Type your pursuit car model --
+Veny.PursuitCar5 = -1711259625           -- Type your pursuit car model --
+Veny.PursuitCar6 = -172996807           -- Type your pursuit car model --
+Veny.PursuitCar7 = 1046936638        -- Type your pursuit car model --
+Veny.PursuitCar8 = -490158933        -- Type your pursuit car model --
+Veny.PursuitCar9 = -689302440        -- Type your pursuit car model --
+Veny.PursuitCar10 = -801473709        -- Type your pursuit car model --
+Veny.PursuitCar11 = 2104988240       -- Type your pursuit car model --
+Veny.PursuitCar12 = -21396939964       -- Type your pursuit car model --
+
+
+Veny.MultiplierAPlus = 0.495000 --- Fast
+Veny.MultiplierS = 1.595000 --- Pretty Fast
+Veny.MultiplierSPlus = 26.695000 --- Fastest
+Veny.MultiplierA = 0.395000 --- Slowest
+
+Veny.notify = "qbcorenotify" --- can be mythicnotify, notification(must use nopixel notification), qbcorenotify, esxshownotification ---
