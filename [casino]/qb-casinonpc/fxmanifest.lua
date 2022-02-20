c92b08f78c6bef6dcf914921c028d49cc908a66d
VPSAccountPieces <100085517+VPSAccountPieces@users.noreply.github.com>
2022-02-20 21:54:50 +0000

    YOU MAKE A WOMAN GO MAD!

---
+++ [casino]/qb-casinonpc/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'QBCore#4011    : Casino NPCs'
+
+version '1.0.0'
+
+client_scripts {
+
+	'client.lua'
+}
---
+++ [casino]/qb-insidetrack/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'QBCore#4011    : Inside-Track'
+
+version '1.0.0'
+
+client_scripts {
+    'client/utils.lua',
+    'client/client.lua',
+    'client/screens/*.lua',
+}
+
+server_script 'server/server.lua'
