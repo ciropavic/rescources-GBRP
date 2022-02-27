835bc356933559ccf99b23b5eedac71be3f7bb8b
ReeceR457 <55110653+ReeceR457@users.noreply.github.com>
2022-02-27 13:37:23 +0000

    very top secret stuff :squirrel:

---
+++ [qb]/nui_doorlock/configs/carmeet.lua
@@ -0,0 +1,20 @@
+
+
+-- carmeetgaragedoor
+table.insert(Config.DoorList, {
+	locked = true,
+	maxDistance = 6.0,
+	objHash = -982531572,
+	objHeading = 85.0,
+	lockpick = false,
+	audioRemote = false,
+	slides = true,
+	objCoords = vector3(950.7746, -1698.227, 31.44471),
+	garage = true,
+	fixText = false,
+	authorizedJobs = { ['dev']=0 },        
+    -- oldMethod = true,
+    -- audioLock = {['file'] = 'metal-locker.ogg', ['volume'] = 0.6},
+    -- audioUnlock = {['file'] = 'metallic-creak.ogg', ['volume'] = 0.7},
+    -- autoLock = 1000
+})
